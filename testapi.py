09009c5f8f889814ecf3622453fc77b07f4a4114
oooo2552 <35589559+oooo2552@users.noreply.github.com>
2021-11-25 15:36:55 +0800

    Update testapi.py

--- testapi.py
+++ testapi.py
@@ -1,28 +1,31 @@
-
+from app import app ##
 import flask
 import camera
 from flask import request
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+#app = flask.Flask(__name__)
+#app.config["DEBUG"] = True
 
 
-defult_address = "/"
-my_address = "/https://oooo2552.github.io/linguisticpuzzle/experiment.html"
+#defult_address = "/"
+#my_address = "/https://oooo2552.github.io/linguisticpuzzle/experiment.html"
 
 @app.route('/')
 def index():
     return "index page"
 
-@app.route('/camera', methods=['GET'])
+@app.route('/camera', methods=['GET','POST'])
 def start_camera():
-    #if request.method == 'POST':
-    #insert_values = request.get_json() 
-    #filename = insert_values['subject']
-    #filename = input("please enter the filename:")
-    filename = "test_heroku"
+    if request.method == 'POST':
+      insert_values = request.get_json() 
+      filename = insert_values['subject']
+    else:
+      #filename = input("please enter the filename:")
+      filename = "test3"
     camera.main(filename)
     return "<h1>This is second camera for facial expression</h1>"
 
 
-app.run()
+#app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=False)
